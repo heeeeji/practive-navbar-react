7dad4b8f94c52655e98aa0326b652864165c3a79
heeeeji <vhcl1004@naver.com>
2020-01-21 13:11:38 +0900

    bootstrap-navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
-import Navbar from "./components/Navbar";
+import Header from "./components/Header";
 
 const App = () => {
   return (
     <>
-      <Navbar />
+      <Header />
       <h1>Hellow React</h1>
       <h2>안녕하세요</h2>
     </>
